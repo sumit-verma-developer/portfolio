179e1bf6d8998778502fbc872f3f3ba915374802
operation-tss <tssoperation@gmail.com>
2024-11-14 19:12:04 +0530

    final project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,25 @@ import { FaHome } from "react-icons/fa";
 import Grid from "../../components/Grid";
 import RecentProjects from "../../components/RecentProjects";
 import { navItems } from "../../data";
+import Clients from "../../components/ui/Clients";
+import Experience from "../../components/ui/Experience";
+import Approach from "../../components/ui/Approach";
+import Footer from "../../components/ui/Footer";
 
 export default function Home() {
-  
   return (
-   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
-    <div className="max-w-7xl w-full">
-    <FloatingNav 
-     navItems={navItems}/>
-     <Hero/>
-     <Grid/>
-    <RecentProjects/>
-    </div>
-   </main>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 overflow-clip">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach/>
+        <Footer/>
+        
+      </div>
+    </main>
   );
 }

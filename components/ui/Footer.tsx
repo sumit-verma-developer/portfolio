ddc7b08365828e5321cfb5f21bda5e6df05229e6
sumit verma <102705125+sumit-verma-developer@users.noreply.github.com>
2026-02-14 00:21:44 +0530

    copy write add

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2025 Sumit Verma
+          Copyright © 2026 Sumit Verma
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
